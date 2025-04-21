2a9d8e9d17989d69de634940a3e11220d7dd1348
chandana1234567774 <ammuchandu2003@gmail.com>
2025-04-21 19:19:26 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,9 @@
+
+let fruits=["apple","pineapple","cherry","Mango"]
+let sortedFruits = fruits.sort()
+console.log("sortedFruits",sortedFruits); 
+let num=[10,20,30,40,50]
+let sortNumAscending= num.sort((a,b)=>a-b) 
+console.log("sortNumAscending",sortNumAscending);
+
+
